e92c99bad8b8cf3544adc9b62f776c1d6dd8a122
Frangu Vlad <kingdgrizzle@gmail.com>
2018-08-02 22:31:59 +0300

    Fix typings and release 1.0.3

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,14 +1,14 @@
 declare module 'chainfetch' {
 
-	import { METHODS as HTTPMethods, Agent as HTTPAgent } from 'http';
+	import { METHODS as NativeHTTPMethods, Agent as HTTPAgent } from 'http';
 	import { Agent as HTTPSAgent } from  'https';
 	import { Stream } from 'stream';
 	import { Headers, Response as FetchResponse } from 'node-fetch';
 
-	export class Chainfetch {
+	export default class Chainfetch {
 		public constructor(method: HTTPMethod, url: URL, options?: ChainfetchOptions);
-		public query(name: Array<string[]> | { [key: string]: value } | string, value?: string): Chainfetch;
-		public set(name: Array<string[]> | { [key: string]: value } | string, value?: string): Chainfetch;
+		public query(name: Array<string[]> | { [key: string]: string } | string, value?: string): Chainfetch;
+		public set(name: Array<string[]> | { [key: string]: string } | string, value?: string): Chainfetch;
 		public attach(name: { [key: string]: GenericValue } | string, value?: GenericValue, filename?: string): Chainfetch;
 		public send(data: FormData | Buffer | Stream | object): Chainfetch;
 		public setRedirect(redirect: boolean): Chainfetch;
@@ -19,49 +19,49 @@ declare module 'chainfetch' {
 		public toJSON(): Chainfetch;
 		public toText(): Chainfetch;
 		public toString(): Chainfetch;
-		public then(resolver: Function, rejecter: Function): Promise<Rsponse>;
+		public then(resolver: Function, rejecter: Function): Promise<Response>;
 		public catch(rejecter: Function): Promise<ResponseError>;
 
-		public acl(url: string, optinos?: ChainfetchOptions): Chainfetch;
-		public bind(url: string, optinos?: ChainfetchOptions): Chainfetch;
-		public checkout(url: string, optinos?: ChainfetchOptions): Chainfetch;
-		public connect(url: string, optinos?: ChainfetchOptions): Chainfetch;
-		public copy(url: string, optinos?: ChainfetchOptions): Chainfetch;
-		public delete(url: string, optinos?: ChainfetchOptions): Chainfetch;
-		public get(url: string, optinos?: ChainfetchOptions): Chainfetch;
-		public head(url: string, optinos?: ChainfetchOptions): Chainfetch;
-		public link(url: string, optinos?: ChainfetchOptions): Chainfetch;
-		public lock(url: string, optinos?: ChainfetchOptions): Chainfetch;
-		public merge(url: string, optinos?: ChainfetchOptions): Chainfetch;
-		public mkactivity(url: string, optinos?: ChainfetchOptions): Chainfetch;
-		public mkcalendar(url: string, optinos?: ChainfetchOptions): Chainfetch;
-		public mkcol(url: string, optinos?: ChainfetchOptions): Chainfetch;
-		public move(url: string, optinos?: ChainfetchOptions): Chainfetch;
-		public notify(url: string, optinos?: ChainfetchOptions): Chainfetch;
-		public options(url: string, optinos?: ChainfetchOptions): Chainfetch;
-		public patch(url: string, optinos?: ChainfetchOptions): Chainfetch;
-		public post(url: string, optinos?: ChainfetchOptions): Chainfetch;
-		public propfind(url: string, optinos?: ChainfetchOptions): Chainfetch;
-		public proppatch(url: string, optinos?: ChainfetchOptions): Chainfetch;
-		public purge(url: string, optinos?: ChainfetchOptions): Chainfetch;
-		public put(url: string, optinos?: ChainfetchOptions): Chainfetch;
-		public rebind(url: string, optinos?: ChainfetchOptions): Chainfetch;
-		public report(url: string, optinos?: ChainfetchOptions): Chainfetch;
-		public search(url: string, optinos?: ChainfetchOptions): Chainfetch;
-		public source(url: string, optinos?: ChainfetchOptions): Chainfetch;
-		public subscribe(url: string, optinos?: ChainfetchOptions): Chainfetch;
-		public trace(url: string, optinos?: ChainfetchOptions): Chainfetch;
-		public unbind(url: string, optinos?: ChainfetchOptions): Chainfetch;
-		public unlink(url: string, optinos?: ChainfetchOptions): Chainfetch;
-		public unlock(url: string, optinos?: ChainfetchOptions): Chainfetch;
-		public unsubscribe(url: string, optinos?: ChainfetchOptions): Chainfetch;
+		public static acl(url: string, optinos?: ChainfetchOptions): Chainfetch;
+		public static bind(url: string, optinos?: ChainfetchOptions): Chainfetch;
+		public static checkout(url: string, optinos?: ChainfetchOptions): Chainfetch;
+		public static connect(url: string, optinos?: ChainfetchOptions): Chainfetch;
+		public static copy(url: string, optinos?: ChainfetchOptions): Chainfetch;
+		public static delete(url: string, optinos?: ChainfetchOptions): Chainfetch;
+		public static get(url: string, optinos?: ChainfetchOptions): Chainfetch;
+		public static head(url: string, optinos?: ChainfetchOptions): Chainfetch;
+		public static link(url: string, optinos?: ChainfetchOptions): Chainfetch;
+		public static lock(url: string, optinos?: ChainfetchOptions): Chainfetch;
+		public static merge(url: string, optinos?: ChainfetchOptions): Chainfetch;
+		public static mkactivity(url: string, optinos?: ChainfetchOptions): Chainfetch;
+		public static mkcalendar(url: string, optinos?: ChainfetchOptions): Chainfetch;
+		public static mkcol(url: string, optinos?: ChainfetchOptions): Chainfetch;
+		public static move(url: string, optinos?: ChainfetchOptions): Chainfetch;
+		public static notify(url: string, optinos?: ChainfetchOptions): Chainfetch;
+		public static options(url: string, optinos?: ChainfetchOptions): Chainfetch;
+		public static patch(url: string, optinos?: ChainfetchOptions): Chainfetch;
+		public static post(url: string, optinos?: ChainfetchOptions): Chainfetch;
+		public static propfind(url: string, optinos?: ChainfetchOptions): Chainfetch;
+		public static proppatch(url: string, optinos?: ChainfetchOptions): Chainfetch;
+		public static purge(url: string, optinos?: ChainfetchOptions): Chainfetch;
+		public static put(url: string, optinos?: ChainfetchOptions): Chainfetch;
+		public static rebind(url: string, optinos?: ChainfetchOptions): Chainfetch;
+		public static report(url: string, optinos?: ChainfetchOptions): Chainfetch;
+		public static search(url: string, optinos?: ChainfetchOptions): Chainfetch;
+		public static source(url: string, optinos?: ChainfetchOptions): Chainfetch;
+		public static subscribe(url: string, optinos?: ChainfetchOptions): Chainfetch;
+		public static trace(url: string, optinos?: ChainfetchOptions): Chainfetch;
+		public static unbind(url: string, optinos?: ChainfetchOptions): Chainfetch;
+		public static unlink(url: string, optinos?: ChainfetchOptions): Chainfetch;
+		public static unlock(url: string, optinos?: ChainfetchOptions): Chainfetch;
+		public static unsubscribe(url: string, optinos?: ChainfetchOptions): Chainfetch;
 
-		public static METHODS: HTTPMethods;
+		public static METHODS: NativeHTTPMethods;
 
 		private options: ChainfetchOptions;
 		private customHandler: string;
 		private error: Error;
-	};
+	}
 
 	class FormData {
 		public constructor();
